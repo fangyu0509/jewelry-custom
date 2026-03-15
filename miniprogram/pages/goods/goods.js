8e0f51ec95e54840120253bd4b5b599b8f43ce80
Jewelry Developer <dev@jewelry-custom.com>
2026-03-15 20:38:24 +0800

    feat: 完成好物页面 (红色搜索框 + 分类列表)

--- miniprogram/pages/goods/goods.js
+++ miniprogram/pages/goods/goods.js
@@ -1,7 +1,20 @@
 // pages/goods/goods.js
 Page({
   data: {
-    
+    categories: [
+      { id: 'crystal', title: '水晶周边', count: 1 },
+      { id: 'accessories', title: '随购小物', count: 0 },
+      { id: 'service', title: '相关服务', count: 1 },
+      { id: 'diff', title: '差价补齐', count: 2 }
+    ]
+  },
+
+  goToCategory(e) {
+    const categoryId = e.currentTarget.dataset.id;
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
   },
 
   onLoad() {
