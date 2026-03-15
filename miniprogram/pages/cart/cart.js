e4a1a06b19c7321029ef385c16d71d03a243d70f
Jewelry Developer <dev@jewelry-custom.com>
2026-03-15 20:36:06 +0800

    feat: 完成购物车空状态页面 (匹配设计图)

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,10 +1,51 @@
 // pages/cart/cart.js
 Page({
   data: {
+    cartCount: 0,
+    totalPrice: '0'
+  },
+
+  goToCustom() {
+    wx.navigateTo({
+      url: '/pages/design/custom/custom'
+    });
+  },
+
+  goToGoods() {
+    wx.switchTab({
+      url: '/pages/goods/goods'
+    });
+  },
+
+  checkout() {
+    if (this.data.cartCount === 0) {
+      wx.showToast({
+        title: '购物车是空的',
+        icon: 'none'
+      });
+      return;
+    }
     
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
   },
 
   onLoad() {
     
+  },
+
+  onShow() {
+    // TODO: 从本地存储或后端获取购物车数据
+    this.updateCartData();
+  },
+
+  updateCartData() {
+    // TODO: 实现购物车数据更新逻辑
+    this.setData({
+      cartCount: 0,
+      totalPrice: '0'
+    });
   }
 });
